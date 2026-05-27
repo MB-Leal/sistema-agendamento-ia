a699d6c70035c8d190f2614090a778e685982035
MB-Leal <marcosbleal26@gmail.com>
2026-05-27 14:54:13 -0300

    feat: atualizada rota do webhook para api oficial da meta

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WhatsAppController;
+use App\Http\Controllers\WhatsAppCloudController; // <-- Mudou para o CloudController
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Rota única e oficial do Webhook para a Evolution API
-Route::post('/whatsapp/webhook', [WhatsAppController::class, 'handleWebhook']);
+// Rota oficial para a WhatsApp Cloud API da Meta (Aceita GET e POST)
+Route::match(['get', 'post'], '/whatsapp/webhook', [WhatsAppCloudController::class, 'handleWebhook']);
