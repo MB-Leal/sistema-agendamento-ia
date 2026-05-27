fc844356e5c57b9f42aea6dfdc431ab1b4e671d4
MB-Leal <marcosbleal26@gmail.com>
2026-05-27 18:00:30 -0300

    ajuste na controller linha do use

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WhatsappController;
+use App\Http\Controllers\WhatsAppController; // <-- Garanta os "A"s maiúsculos aqui!
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Rota oficial para a WhatsApp Cloud API da Meta (Aceita GET e POST)
-Route::match(['get', 'post'], '/whatsapp/webhook', [WhatsappController::class, 'handleWebhook']);
+// Rota oficial para o Webhook da Meta (Garantindo o WhatsAppController)
+Route::match(['get', 'post'], '/whatsapp/webhook', [WhatsAppController::class, 'handleWebhook']);
