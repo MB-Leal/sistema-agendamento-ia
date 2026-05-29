82b0a0137c91e8500f7cb1d766702e9265db501b
MB-Leal <marcosbleal26@gmail.com>
2026-05-28 23:38:48 -0300

    ajuste no meodel

--- app/Models/WhatsAppMessage.php
+++ app/Models/WhatsAppMessage.php
@@ -8,3 +8,23 @@ class WhatsAppMessage extends Model
 {
     protected $fillable = ['remote_jid', 'message', 'from_me'];
 }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class WhatsAppMessage extends Model
+{
+    use HasFactory;
+
+    // 💡 IMPORTANTE: Força o Laravel a usar o nome correto da tabela no MySQL
+    protected $table = 'whatsapp_messages';
+
+    protected $fillable = [
+        'remote_jid', 
+        'message', 
+        'from_me'
+    ];
+}
